98c130f924f63ab802e8f997c3b509c0ff1bb0de
Tokubara <1270802817@qq.com>
2020-03-14 16:21:56 +0800

    初始的样子，未作修改

--- CMakeFiles/CacheSim.dir/DependInfo.cmake
+++ CMakeFiles/CacheSim.dir/DependInfo.cmake
@@ -4,10 +4,10 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/embedded/文档/华为/simulator/CacheSim.cpp" "/home/embedded/文档/华为/simulator/CMakeFiles/CacheSim.dir/CacheSim.cpp.o"
-  "/home/embedded/文档/华为/simulator/main.cpp" "/home/embedded/文档/华为/simulator/CMakeFiles/CacheSim.dir/main.cpp.o"
+  "/Users/baiyunren/Desktop/计算机系统结构/实验project/CacheExpSrc/CacheSim.cpp" "/Users/baiyunren/Desktop/计算机系统结构/实验project/CacheExpSrc/CMakeFiles/CacheSim.dir/CacheSim.cpp.o"
+  "/Users/baiyunren/Desktop/计算机系统结构/实验project/CacheExpSrc/main.cpp" "/Users/baiyunren/Desktop/计算机系统结构/实验project/CacheExpSrc/CMakeFiles/CacheSim.dir/main.cpp.o"
   )
-set(CMAKE_CXX_COMPILER_ID "GNU")
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
 
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
